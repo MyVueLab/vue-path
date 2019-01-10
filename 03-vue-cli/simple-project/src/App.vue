63e78bda65af2d7ce7c95de32b3c1722bda88c34
simaodeveloper <simaodeveloper@gmail.com>
2019-01-10 08:43:06 -0200

    Add more lessons

---
+++ 03-vue-cli/simple-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: 'Data test with VueJS'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    background-color: orangered;
+    color: #fff;
+  }
+</style>
